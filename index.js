716c8b8237864fbb8b6d56b2acf417640132cfd0
ghtryrty <msjcehdiromdane@hotmail.com>
2024-05-11 17:40:35 +0700

    update sdk commit

--- index.js
+++ index.js
@@ -1,17 +1,1 @@
-function combinationSum3(k, n) {
-  const result = [];
-  backtrack([], 1, k, n);
-  return result;
-  function backtrack(combination, start, k, n) {
-    if (n === 0 && k === 0) {
-      result.push([...combination]);
-      return;
-    }
-    if (n < 0 || k === 0) return;
-    for (let i = start; i <= 9; i++) {
-      combination.push(i);
-      backtrack(combination, i + 1, k - 1, n - i);
-      combination.pop();
-    }
-  }
-}
+const isPalindrome = (str) => str === str.split("").reverse().join("");
