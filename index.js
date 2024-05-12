dc2a0c77d75f350a2f411ab34c7b3f879ba87e8a
ghtryrty <msjcehdiromdane@hotmail.com>
2024-05-12 12:59:39 +0700

    test project

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-const isPalindrome = (str) => str === str.split("").reverse().join("");
+function findRedundantConnection(edges) {
+  const parent = new Array(edges.length + 1).fill(-1);
+  for (const edge of edges) {
+    const u = find(parent, edge[0]);
+    const v = find(parent, edge[1]);
+    if (u === v) return edge;
+    parent[u] = v;
+  }
+  return [];
+}
+function find(parent, i) {
+  if (parent[i] === -1) return i;
+  return find(parent, parent[i]);
+}
