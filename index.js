9f84c94107726fd560d4879fcb24359f880393b4
ghtryrty <msjcehdiromdane@hotmail.com>
2024-05-13 11:16:12 +0700

    feat project commit

--- index.js
+++ index.js
@@ -1,14 +1,4 @@
-function findRedundantConnection(edges) {
-  const parent = new Array(edges.length + 1).fill(-1);
-  for (const edge of edges) {
-    const u = find(parent, edge[0]);
-    const v = find(parent, edge[1]);
-    if (u === v) return edge;
-    parent[u] = v;
-  }
-  return [];
-}
-function find(parent, i) {
-  if (parent[i] === -1) return i;
-  return find(parent, parent[i]);
-}
+const factorial = (num) => {
+  if (num === 0) return 1;
+  return num * factorial(num - 1);
+};
